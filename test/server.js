0ce2618ac2f4c11b5cb5c80c5728963f10b4a08a
Andrea Imparato <imparato.andrea@gmail.com>
2013-11-11 17:43:48 +0100

    starting to test

---
+++ test/server.js
@@ -0,0 +1,95 @@
+var request = require('supertest'),
+    assert = require('assert'),
+    app = require('../app'),
+    mongoose = require('mongoose'),
+    dburl = "mongodb://localhost:27017/link_db",
+    //automatic db cleaning
+    clearDB = require('mocha-mongoose')(dburl),
+    LinkModel = mongoose.model('Link', app.Link),
+    id;
+
+// #############################################################################
+// Api testing
+// #############################################################################
+
+
+
+// Insert records before the tests
+beforeEach(function(done) {
+
+    console.log('Inserting data');
+    mongoose.createConnection(dburl);
+    //Models
+    var link = new LinkModel({
+        url: "url",
+        description: "description"
+    });
+
+    link.save(function(err, model) {
+
+        LinkModel.find({}, function(err, links) {
+            if (err) return done(err);
+            assert.equal(links.length, 1);
+            id = links[0].id;
+            console.log(id);
+        });
+    });
+
+    done();
+
+});
+
+
+
+describe('Server testing', function() {
+
+    describe('GET /', function() {
+        it('responds with index.html', function(done) {
+            request(app)
+                .get('/')
+                .end(function(err, res) {
+                    // Make sure there was no error
+                    assert.equal(err, null);
+                    // Finish asynchronous test
+                    done();
+                });
+        });
+    });
+
+    describe('GET /api/links', function() {
+        it('responds json links', function(done) {
+            request(app)
+                .get('/api/links')
+                .end(function(err, res) {
+                    // Make sure there was no error
+                    assert.equal(err, null);
+                    assert.notEqual(res, null);
+                    assert.equal(res.body.length, 1);
+                    assert.equal(res.body[0].url, 'url');
+                    assert.equal(res.body[0].description, 'description');
+                    // Finish asynchronous test
+                    done();
+                });
+        });
+
+    });
+
+
+    describe('GET /api/links/:id', function() {
+        it('responds json link', function(done) {
+            request(app)
+                .get('/api/links/' + id)
+                .end(function(err, res) {
+                    assert.equal(err, null);
+                    console.log(res.body);
+                    // Make sure there was no error
+                    // Finish asynchronous test
+                    done();
+                });
+        });
+
+    });
+
+
+
+});
